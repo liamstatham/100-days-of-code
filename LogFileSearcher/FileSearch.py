0e5dfa2ce409d7b7badbd82d05e1176e52284f86
Liam Statham <liamstatham@gmail.com>
2021-01-04 20:44:52 +0000

    Added datetime conversion to first 2 strings to create a correct datetime log.

--- LogFileSearcher/FileSearch.py
+++ LogFileSearcher/FileSearch.py
@@ -1,5 +1,5 @@
 import sqlite3
-import datetime
+from datetime import datetime
 #Create sqlite database
 #conn = sqlite3.connect('logdb.sqlite')
 #cur = conn.cursor()
@@ -23,11 +23,14 @@ records = 0
 for lines in file:
     if not lines.startswith('2021'):continue
     line = lines.split()
-#need to convert line[0] and line[1] from string to datetime    
-#    dt = datetime.datetime(line[0])
-#    tm = datetime.time(line[1])
-#    combine = datetime.combine(dt, tm)
-    print(line[0], line[1], combine)
+#need to convert line[0] and line[1] from string to datetime
+    bigdate = line[0]
+    time = line[1]
+    fulldate = (bigdate + ' ' + time)
+    dt = datetime.fromisoformat(fulldate)
+#Above converts part 0 and 1 from log file line to a full date. using datetime 
+#    print(type(line[1]))
+    print(dt)
     records = records + 1
 print('Number of records to enter into database: ',records)
 #splits file into lines beginging with 2021
