66d29d696f4f1b788fb8b32f8cabda6b5d461ea5
Liam Statham <liamstatham@gmail.com>
2021-01-05 15:58:41 +0000

    Worked some more on the log search project.   - Split the lines into correct pieces of information and added each to a variable   - Converted some to different types for the database   - Added a SQL script to make sure that no duplicate files will be added to the database, this was a tricky one as there is often nothing specifically different between lines - so ended up using 3 pieces of information to make sure.   - Added SQL to insert records into table   - Updated table to reflect relevant information in log file   - Tested with a small and full size log file.

--- LogFileSearcher/FileSearch.py
+++ LogFileSearcher/FileSearch.py
@@ -1,18 +1,23 @@
 import sqlite3
 from datetime import datetime
 #Create sqlite database
-#conn = sqlite3.connect('logdb.sqlite')
-#cur = conn.cursor()
+conn = sqlite3.connect('logdb.sqlite')
+cur = conn.cursor()
 
 #Create logs table
-#cur.execute('''
-#CREATE TABLE if not exists Logs (
-#    LogID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-#    FromFile TEXT,
-#    Datetime DATETIME,
-#    FromIP TEXT,
-#    FullText TEXT
-#); ''')
+cur.execute('''
+CREATE TABLE if not exists Logs (
+    LogID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    Created DATETIME,
+    Method TEXT,
+    UriStem TEXT,
+    UriQuery TEXT,
+    Port INTEGER,
+    IP TEXT,
+    UserAgent TEXT,
+    URL TEXT,
+    TimeTaken INTEGER
+); ''')
 
 #Open file
 fname = input('Enter file name: ')
@@ -21,6 +26,7 @@ file = open(fname)
 
 records = 0
 for lines in file:
+#splits file into lines beginging with 2021
     if not lines.startswith('2021'):continue
     line = lines.split()
 #need to convert line[0] and line[1] from string to datetime
@@ -28,12 +34,32 @@ for lines in file:
     time = line[1]
     fulldate = (bigdate + ' ' + time)
     dt = datetime.fromisoformat(fulldate)
-#Above converts part 0 and 1 from log file line to a full date. using datetime 
-#    print(type(line[1]))
-    print(dt)
+#Above converts part 0 and 1 from log file line to a full date. using datetime
+    method = line[3]
+    uristem = line[4]
+    uriquery = line[5]
+    port = int(line[6])
+    ip = line[8]
+    useragent = line[9]
+    url = line[10]
+    timetaken = int(line[16])
+#Number of records being added to table
     records = records + 1
-print('Number of records to enter into database: ',records)
-#splits file into lines beginging with 2021
+#Add new records to table if they don't already exist in the database
+    cur.execute('''SELECT Created, Method, UriStem FROM Logs WHERE Created = ? AND Method = ? AND UriStem = ?''', (dt, method, uristem))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute(
+        '''INSERT INTO Logs (Created, Method, UriStem, UriQuery, Port, IP, UserAgent, URL, TimeTaken)
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+        (dt, method, uristem, uriquery, port, ip, useragent, url, timetaken))
+    else:
+        print(records)
+    conn.commit()
 
-
-#print(counts)
+print('Number of records added to database: ',records)
+#logfile info
+#Fields: (0 date) (1 time) (2 s-ip) (3 cs-method) (4 cs-uri-stem + cs-uri-query)
+#(6 s-port) (7 cs-username) (8 c-ip) (9 cs(User-Agent))
+#(10 cs(Referer)) (11 sc-status) (12 sc-substatus)
+#(13 sc-win32-status) (14 sc-bytes) (15 cs-bytes) (16 time-taken)
--- log.md
+++ log.md
@@ -81,6 +81,14 @@ Signing off 20:30 (had a bad nights sleep last night, so trying to change routin
 **Today's Progress**: Feels like a big day, started my log file searcher project. I've uploaded the start to the master branch under folder "LogFileSearcher". It currently creates a SQLite database, with a relevant table, parses through each relevant line in the file and splits the line into relevant chunks; such as date, time, fromIP, full text etc. It's using the datetime module to convert the date and time strings into a datetime, so I can sort the database by time correctly. Really pleased with the challenges I've overcome today.
 Didn't continue the data viz examples as they were a bit boring.
 
-**Issues overcame**: Loop through a file with random line breaks in the wrong place. Count lines correctly, count parts of line correctly. Concat two string parts (year-month-date) and (time:time:time) and then convert to one datetime variable. 
+**Issues overcame**: Loop through a file with random line breaks in the wrong place. Count lines correctly, count parts of line correctly. Concat two string parts (year-month-date) and (time:time:time) and then convert to one datetime variable.
 
 ### Day 5: Jan 05, Tuesday
+
+**Today's Progress**: Worked some more on the log search project.
+  - Split the lines into correct pieces of information and added each to a variable
+  - Converted some to different types for the database
+  - Added a SQL script to make sure that no duplicate files will be added to the database, this was a tricky one as there is often nothing specifically different between lines - so ended up using 3 pieces of information to make sure.
+  - Added SQL to insert records into table
+  - Updated table to reflect relevant information in log file
+  - Tested with a small and full size log file. 
